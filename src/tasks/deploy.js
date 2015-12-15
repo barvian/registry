3fa60719fe18fcedf4e2ac5763e75eaa56cf03ad
Maxwell Barvian <max@barvian.me>
2015-12-15 13:52:14 -0600

    No more default deploy function for some reason

--- src/tasks/deploy.js
+++ src/tasks/deploy.js
@@ -2,10 +2,10 @@ import * as deploys from './deploys';
 
 export function load(gulp, config) {
   gulp.task('deploy', ['build'], (cb) => {
-    deploys[config.type](cb, config);
+    deploys[config.type].deploy(cb, config);
   });
 
   if (config.type === 'rsync' && config.syncable) {
-    gulp.task('sync', (cb) => deploys.rsync(cb, config, true));
+    gulp.task('sync', (cb) => deploys.rsync.deploy(cb, config, true));
   }
 };
