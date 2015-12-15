8ad0c910fd94fd3c99c039d75d00116329b9188c
Maxwell Barvian <max@barvian.me>
2015-12-14 19:00:41 -0600

    Better default export

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 import * as tasks from './tasks';
 
-export default function load(gulp, config) {
+export function load(gulp, config) {
   config = Object.assign({build: null, clean: null, watch: null}, config);
   Object.keys(tasks).filter(task => config.hasOwnProperty(task)).forEach(task =>
     tasks[task].load(gulp, config[task])
@@ -11,3 +11,5 @@ export default function load(gulp, config) {
 
   gulp.task('default', ['build', 'watch']);
 };
+
+export default load;
