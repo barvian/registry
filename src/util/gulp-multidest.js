11f7073e681676f435db9fc37641d388c2db4e2f
Maxwell Barvian <max@barvian.me>
2016-01-03 17:34:13 -0600

    Simplify gulp-multidest

--- src/util/gulp-multidest.js
+++ src/util/gulp-multidest.js
@@ -2,9 +2,9 @@ import gulp from 'gulp';
 import lazypipe from 'lazypipe';
 import flatten from 'array-flatten';
 
-export default function multidest(dests) {
+export default function multidest(...dests) {
   let pipeline = lazypipe();
-  flatten([dests]).forEach(function(dest) {
+  flatten(dests).forEach(function(dest) {
     pipeline = pipeline.pipe(gulp.dest, dest);
   });
   return pipeline();
